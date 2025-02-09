f0a9c6f0bc62ef7d8878fbc402cfb9499fb794be
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-02-09 10:26:28 +0100

    alarm1

--- clock.ino
+++ clock.ino
@@ -1,22 +1,39 @@
 #include <Arduino.h>
 #include <UTFT.h>                         	//Libriary for TFT
 #include <DS3231.h>                       	//Libriary for Real Time Clock
+#include <EEPROM.h>
 
-// instances 
+//----------- instances-----------------
 UTFT    myGLCD(CTE32_R2, 38, 39, 40, 41);	// myGLCD object of class UTFT with paramenter for specific TFT screed model
 DS3231  rtc(SDA, SCL);                  	// rtc object of class DS3231 - Real-time-clock HW modul connected to pins SDA and SCL
 
-//variables
+//------------ variables ----------------
 extern uint8_t BigFont[];          // Declare which fonts we will be using for TFT
 extern uint8_t SmallFont[];        
 extern uint8_t SevenSegNumFont[];  
 Time t;
-int x, y, hour, minute, minuteBefore, sec, secBefore, alarm_hour, alarm_minute;
+int x, y, hour, minute, minuteBefore, sec, secBefore, alarmHour, alarmMinute, alarmActive, alarmDuration, relayHour, relayMinute, relayActive, relayDuration; 
 int centerX = 120;		// center of round clock
 int centerY = 120;		// center of round clock
 int r = 100;			// radius of round clock
 float a, tempr;
 String message = "";
+
+// ----------- pins ----------
+#define alarmPin   A0
+#define relayPin   A1
+
+//------------ EEPROM map -------------------
+
+#define alarmActiveAdr   31      // Alarm Status
+#define alarmHourAdr     32      // Alarm Hours
+#define alarmMinuteAdr   33      // Alarm Minutes
+#define alarmDurationAdr 34      // Alarm Duration
+
+#define relayActiveAdr   41      // Relay Status
+#define relayHourAdr     42      // Relay Hours
+#define relayMinuteAdr   43      // Relay Minutes
+#define relayDurationAdr 44      // Relay Duration
 
 // routines
 int readSerial(String request) {
@@ -91,6 +108,18 @@ void printInfo() {
   myGLCD.printNumI(minute, x+16*3, y, 2, '0');
   myGLCD.print(":", x+16*5, y);
   myGLCD.printNumI(sec, x+16*6, y, 2, '0');
+  
+   // Print Alarm info
+	y = 200;
+  if (alarmActive) {
+	myGLCD.print("Alarm:", x+16*3, y);
+	myGLCD.printNumI(alarmHour, x+16*3, y+16, 2, '0');
+	myGLCD.print(":", x+16*5, y+16);
+	myGLCD.printNumI(alarmMinute, x+16*6, y+16, 2, '0');
+  } else {
+	myGLCD.print("     ", x+16*3, y);
+	myGLCD.print("     ", x+16*3, y+16);
+  }
 
   // Print temp
   tempr = rtc.getTemp();
@@ -101,8 +130,15 @@ void printInfo() {
   myGLCD.printNumI(tempr, x, y, 2);
   myGLCD.print("*C", x+16*2, y);
   
-
 }
+
+void alarms() {
+	if (alarmActive && (alarmHour == hour) && (alarmMinute == minute)) {
+		tone(alarmPin, 880, 200);
+		delay(300);	
+	} else { digitalWrite(alarmPin, LOW);}
+}
+	
 
 // ------------------------------------
 /* int readSerial2(String request) {
@@ -119,32 +155,58 @@ void printInfo() {
 
 //--------------------------------------------
 void setup() {
-  rtc.begin();							// RTC clock initialisation
-  
-  Serial.begin(9600);					// serial interface initialisation
-  Serial.println("Serial.begin(9600)");
-  
-  myGLCD.InitLCD(LANDSCAPE);			// LCD initialisation
-  myGLCD.clrScr();
-  myGLCD.setBackColor(0, 0, 0);  
-  
-  Serial.println("-- Time Initial Setup --");	// set parameters via serial interface
-  hour = readSerial("Enter hours");				// if hours do not entered during 5 secund readSerial returns "-1",  skip time setup and use time saved in RTC
-    if (hour >= 0) {
-    minute = readSerial("Enter minutes");
-    sec = readSerial("Enter secundes");
-//    alarm_hour = readSerial("Enter alarm_hour");
-//    alarm_minute = readSerial("Enter alarm_minute");
+	rtc.begin();							// RTC clock initialisation
 
-	String message = "";
-	message = message + "-- setting time " + hour + ":" + minute + ":" + sec;
-	Serial.println(message);
+	Serial.begin(9600);					// serial interface initialisation
+	Serial.println("Serial.begin(9600)");
+
+	myGLCD.InitLCD(LANDSCAPE);			// LCD initialisation
+	myGLCD.clrScr();
+	myGLCD.setBackColor(0, 0, 0);  
+
+	Serial.println("-- Time Initial Setup --");	// set parameters via serial interface
+	hour = readSerial("Enter hours");				// if hours do not entered during 5 secund readSerial returns "-1",  skip time setup and use time saved in RTC
+    if (hour >= 0) {
+		minute = readSerial("Enter minutes");
+		sec = readSerial("Enter secundes");
+	//    alarm_hour = readSerial("Enter alarm_hour");
+	//    alarm_minute = readSerial("Enter alarm_minute");
+
+		String message = "";
+		message = message + "-- setting time " + hour + ":" + minute + ":" + sec;
+		Serial.println(message);
+	  
+		rtc.setTime(hour, minute, sec);
+	}
+	
+	EEPROM.update(alarmActiveAdr, 0);	// Alarm Status ON
+	EEPROM.update(alarmHourAdr, 7);		// Alarm Hours
+	EEPROM.update(alarmMinuteAdr, 0);	// Alarm Minutes
+	//EEPROM.update(alarmDurationAdr, 1);	// Alarm Duration minutes
+
+	//EEPROM.update(relayActiveAdr, 1);	// Relay Status ON
+	//EEPROM.update(relayHourAdr, 6);		// Relay Hours
+	//EEPROM.update(relayMinuteAdr, 50);	// Relay Minutes
+	//EEPROM.update(relayDurationAdr, 10); 
   
-    rtc.setTime(hour, minute, sec);
-  }
-  drawClockface();
-  Serial.println("setup routine done");
-  //tone(A0, 440, 100);
+	alarmActive		= EEPROM.read(alarmActiveAdr);
+	alarmHour		  = EEPROM.read(alarmHourAdr);
+	alarmMinute		= EEPROM.read(alarmMinuteAdr);
+	//alarmDuration	= EEPROM.read(alarmDurationAdr, 1);
+	
+	//relayActive		= EEPROM.read(relayActiveAdr, 1);
+	//relayHour		= EEPROM.read(relayHourAdr, 6);
+	//relayMinute		= EEPROM.read(relayMinuteAdr, 50);
+	//relayDuration	= EEPROM.read(relayDurationAdr, 10); 
+	
+	// temporal code
+	t = rtc.getTime();
+	alarmHour = t.hour;
+	alarmMinute = t.min + 1;
+	
+	drawClockface();
+	Serial.println("setup routine done");
+	//tone(A0, 440, 100);
 }
 // ---------------------------------------
 void loop() {
@@ -162,4 +224,6 @@ void loop() {
     printInfo();
     secBefore = sec;
   }
+  
+  alarms();
 }
