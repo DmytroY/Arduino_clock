7cc314924d29f03ed69e21893bb898d7aad1b7f1
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-06-16 13:53:48 +0200

    Celsius

--- clock.ino
+++ clock.ino
@@ -12,33 +12,20 @@ extern uint8_t BigFont[];          // Declare which fonts we will be using for T
 extern uint8_t SmallFont[];        
 extern uint8_t SevenSegNumFont[];  
 Time t;
-int x, y, hour, minute, minuteBefore, sec, secBefore, alarmHour, alarmMinute, alarmActive, alarmDuration, relayHour, relayMinute, relayActive, relayDuration; 
-int centerX = 120;		// center of round clock
+int x, y, hour, minute, minuteBefore, sec, secBefore, date, month, year; 
+int centerX = 110;		// center of round clock
 int centerY = 120;		// center of round clock
 int r = 100;			// radius of round clock
 float a, tempr;
 String message = "";
-
-// ----------- pins ----------
-#define alarmPin   A0
-#define relayPin   A1
-
-//------------ EEPROM map -------------------
-
-#define alarmActiveAdr   31      // Alarm Status
-#define alarmHourAdr     32      // Alarm Hours
-#define alarmMinuteAdr   33      // Alarm Minutes
-#define alarmDurationAdr 34      // Alarm Duration
-
-#define relayActiveAdr   41      // Relay Status
-#define relayHourAdr     42      // Relay Hours
-#define relayMinuteAdr   43      // Relay Minutes
-#define relayDurationAdr 44      // Relay Duration
+String month_str = "";
+String month_last = "";
 
 // routines
+//----------------------------------------------------------------------
 int readSerial(String request) {
 // the function for reading serial.
-// wiil be used for initial setup hours, minutes and secundes
+// wiil be used for initial setup time and date
 // In case of hours do not entered during 5 sec returns -1
   Serial.println(request);
   String inputText = "";
@@ -48,9 +35,10 @@ int readSerial(String request) {
     if (Serial.available()) {
       inputText = Serial.readString();
     }
-    if ((request == "Enter hours") && (millis() - startTime > 5000)) return -1;
+    if ((request == "Enter year") && (millis() - startTime > 5000)) return -1;
   }
   inputText.trim();                        // remove any \r \n whitespace at the end of the String
+  Serial.println(inputText);
   return inputText.toInt();
 }
 
@@ -63,7 +51,7 @@ void drawClockface(){
 	}
 }
 	
-
+//--------------------------------------------------------------------
 void drawHands() {
   int hourHandL = r*6/10;
   int hourHandW = 3;
@@ -96,7 +84,7 @@ void drawHands() {
   }
   
 }
-
+//------------------------------------------------------------------
 void printInfo() {
   // Print time
   x = 190;
@@ -108,50 +96,37 @@ void printInfo() {
   myGLCD.printNumI(minute, x+16*3, y, 2, '0');
   myGLCD.print(":", x+16*5, y);
   myGLCD.printNumI(sec, x+16*6, y, 2, '0');
-  
-   // Print Alarm info
-	y = 200;
-  if (alarmActive) {
-	myGLCD.print("Alarm:", x+16*3, y);
-	myGLCD.printNumI(alarmHour, x+16*3, y+16, 2, '0');
-	myGLCD.print(":", x+16*5, y+16);
-	myGLCD.printNumI(alarmMinute, x+16*6, y+16, 2, '0');
-  } else {
-	myGLCD.print("     ", x+16*3, y);
-	myGLCD.print("     ", x+16*3, y+16);
-  }
 
-  // Print temp
+  // Print data to serial
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(minute);
+  Serial.print(":");
+  Serial.print(sec);
+  
   tempr = rtc.getTemp();
-  Serial.print("-- Temperature: ");
-  Serial.println(tempr);
+  Serial.print("   ");
+  Serial.print(tempr);
+  Serial.println("°C");
+  
   x = 250;
   y = 40;
   myGLCD.printNumI(tempr, x, y, 2);
-  myGLCD.print("*C", x+16*2, y);
+  myGLCD.print("C", x+16*3-4, y);
+  myGLCD.drawCircle(x+16*2+8, y+4, 3);
+  myGLCD.drawCircle(x+16*2+8, y+4, 2);
   
+  // Print date
+  y = 168;
+  
+  myGLCD.printNumI(date, x + 16*2 , y, 2);
+  if(month_str != month_last) {
+	  myGLCD.print("         ", x + 16*4 - 16 * 9, y + 8*3);
+	  month_last = month_str;
+  } 
+  myGLCD.print(month_str, x + 16*4 - 16 * month_str.length(), y + 8*3);
+  myGLCD.printNumI(year, x , y + 8*6);
 }
-
-void alarms() {
-	if (alarmActive && (alarmHour == hour) && (alarmMinute == minute)) {
-		tone(alarmPin, 880, 200);
-		delay(300);	
-	} else { digitalWrite(alarmPin, LOW);}
-}
-	
-
-// ------------------------------------
-/* int readSerial2(String request) {
-   Serial.println(request);
-   String inputText = "";
-   while (inputText == ""){
-    if (Serial.available()) {
-      inputText = Serial.readString();
-    }
-  }
-  inputText.trim();            
-  return inputText.toInt();
-} */
 
 //--------------------------------------------
 void setup() {
@@ -164,45 +139,25 @@ void setup() {
 	myGLCD.clrScr();
 	myGLCD.setBackColor(0, 0, 0);  
 
-	Serial.println("-- Time Initial Setup --");	// set parameters via serial interface
-	hour = readSerial("Enter hours");				// if hours do not entered during 5 secund readSerial returns "-1",  skip time setup and use time saved in RTC
-    if (hour >= 0) {
+	Serial.println("-- Date and Time Initial Setup --");	// set parameters via serial interface
+	year =readSerial("Enter year"); 	// if year did not entered during 5 secund readSerial returns "-1",  skip time setup and use time saved in RTC
+    if (year >= 0) {
+		month = readSerial("Enter month");
+		date = readSerial("Enter day of month");
+		hour = readSerial("Enter hour");
 		minute = readSerial("Enter minutes");
 		sec = readSerial("Enter secundes");
-	//    alarm_hour = readSerial("Enter alarm_hour");
-	//    alarm_minute = readSerial("Enter alarm_minute");
-
-		String message = "";
+		
+		message = "";
+		message = message + "-- setting date " + date + " " + month + " " + year;
+		Serial.println(message);
+		rtc.setDate(date, month, year);	
+		
+		message = "";
 		message = message + "-- setting time " + hour + ":" + minute + ":" + sec;
 		Serial.println(message);
-	  
 		rtc.setTime(hour, minute, sec);
 	}
-	
-	EEPROM.update(alarmActiveAdr, 0);	// Alarm Status ON
-	EEPROM.update(alarmHourAdr, 7);		// Alarm Hours
-	EEPROM.update(alarmMinuteAdr, 0);	// Alarm Minutes
-	//EEPROM.update(alarmDurationAdr, 1);	// Alarm Duration minutes
-
-	//EEPROM.update(relayActiveAdr, 1);	// Relay Status ON
-	//EEPROM.update(relayHourAdr, 6);		// Relay Hours
-	//EEPROM.update(relayMinuteAdr, 50);	// Relay Minutes
-	//EEPROM.update(relayDurationAdr, 10); 
-  
-	alarmActive		= EEPROM.read(alarmActiveAdr);
-	alarmHour		  = EEPROM.read(alarmHourAdr);
-	alarmMinute		= EEPROM.read(alarmMinuteAdr);
-	//alarmDuration	= EEPROM.read(alarmDurationAdr, 1);
-	
-	//relayActive		= EEPROM.read(relayActiveAdr, 1);
-	//relayHour		= EEPROM.read(relayHourAdr, 6);
-	//relayMinute		= EEPROM.read(relayMinuteAdr, 50);
-	//relayDuration	= EEPROM.read(relayDurationAdr, 10); 
-	
-	// temporal code
-	t = rtc.getTime();
-	alarmHour = t.hour;
-	alarmMinute = t.min + 1;
 	
 	drawClockface();
 	Serial.println("setup routine done");
@@ -214,6 +169,10 @@ void loop() {
   hour = t.hour;
   minute = t.min;
   sec = t.sec;
+  date = t.date;
+  year = t.year;
+  
+  month_str = rtc.getMonthStr();
 
   if (minute != minuteBefore) {
 	  drawHands();
@@ -225,5 +184,4 @@ void loop() {
     secBefore = sec;
   }
   
-  alarms();
 }
